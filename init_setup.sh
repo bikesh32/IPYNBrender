a7a724497673e4518830bf3b3f68137f4c14b5e2
bikesh32 <bikeshsingh023@gmail.com>
2023-08-14 18:02:00 +0530

    upadate

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START"
 echo [$(date)]: "Creating conda env with python version 3.8"
-conda create  -p/--prefix ./env python=3.8 -y
+conda create  -p/--prefix ./venv python=3.8 -y
 echo [$(date)]: "activate env"
-source activate ./env
+source activate ./venv
 echo [$(date)]: "installing dev requirements"
 pip install -r requirements_dev.txt
 echo [$(date)]: "END"
